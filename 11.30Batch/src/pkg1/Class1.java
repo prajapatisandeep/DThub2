5b7f7527cde1f4cd9e1ce00a39934ed416c6fb50
Dell <Dell@DESKTOP-30FNJH5>
2019-02-16 20:55:52 -0800

    Push the code

---
+++ 11.30Batch/src/pkg1/Class1.java
@@ -0,0 +1,25 @@
+package pkg1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Class1
+{
+public static void main(String[] args) 
+{
+System.setProperty("webdriver.chrome.driver","C:\\Users\\Dell\\Desktop\\chromedriver.exe");	
+ChromeDriver deepak=new ChromeDriver();
+deepak.get("https://www.facebook.com");
+deepak.manage().window().maximize();
+
+WebElement user =deepak.findElement(By.id("email"));	
+user.sendKeys("Deepak");
+WebElement password=deepak.findElement(By.cssSelector("input#pass"));
+password.sendKeys("chanana");
+WebElement login=deepak.findElement(By.id("loginbutton"));
+login.click();
+}	
+	
+
+}
---
+++ 11.30Batch/src/pkg1/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Dell
+ *
+ */
+package pkg1;
